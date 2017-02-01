717d3e5312062d3cc0568c2839ef529798317366
Damjan Miklic <damjan.miklic@gmail.com>
2017-02-01 12:32:47 +0100

    Update sounds (downloaded from gdrive).

--- Adore intro/Sounds/download_sounds.sh
+++ Adore intro/Sounds/download_sounds.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 wget -O bok.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoR0p0TjlqcTJMUTg"
+
+wget -O dodji.wav "https://drive.google.com/uc?export=download&id="
+
+wget -O dodji_se_igrati.wav "https://drive.google.com/uc?export=download&id="
+
+wget -O nesto_cu_ti_pokazati.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoNHBEeXZzSHcydHM"
+
+wget -O nadam_se_da_cemo_se_opet_igrati.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoeDBzRlhXSXpGY0E"
+
+wget -O moram_napuniti_baterije.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoR0p0TjlqcTJMUTg"
+
+wget -O ja_sam_robot_rene.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoeGhNWVViVWJ3ZkU"
+
+wget -O vidi_sto_imam.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoczloX1hGanhVN1E"
+
+wget -O sada_se_moram_malo_odmoriti.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoM0I0M2pxMmJ4M2c"
+
+wget -O pa_pa_rate02e.wav "https://drive.google.com/uc?export=download&id=0ByIZc3t3wyJoWWM4XzMtOENuOUU"
+
